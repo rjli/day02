957e0af5a3625aef3c3f587cf871429c998eb198
rjli <1556913110@qq.com>
2015-09-16 10:26:02 +0800

    Update ServletDemo1.java
    Add from Web

--- src/cn/itcast/jsp/ServletDemo1.java
+++ src/cn/itcast/jsp/ServletDemo1.java
@@ -10,12 +10,14 @@ import javax.servlet.http.HttpServletResponse;
 
 public class ServletDemo1 extends HttpServlet {
 
-	
+	//xinjnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
 		String username=request.getParameter("username");
 		System.out.println(username);
+		System.out.println("Add from web can you receive me !");
+		
 	}
 	
 
